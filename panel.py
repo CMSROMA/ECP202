9f1e54fe2fd85933ae5b5f3e96433df859c0ac10
cmsdaq <cmsdaq@pccmsdaq03.localdomain>
2021-06-16 18:56:10 +0200

    adding evaporator temperature

--- panel.py
+++ panel.py
@@ -38,11 +38,12 @@ class ECP202ControlPanel(npyscreen.ActionForm):
             self.fanSwitch.value=False
         self.tset_target=self.ecp202.getTargetTemperature()
         self.tset = self.add(npyscreen.TitleText, name="TSET:", value=str(self.tset_target), rely=9)
-        self.tmon = self.add(npyscreen.TitleText, name="T       :", value="", editable=False, rely=11)
-        self.compressorStatus = self.add(npyscreen.TitleText, name="COMPR :", value="", editable=False)
-        self.fanStatus = self.add(npyscreen.TitleText, name="FAN :", value="", editable=False)
-        self.defrostStatus = self.add(npyscreen.TitleText, name="DEFROST :", value="", editable=False)
-        self.alarm = self.add(npyscreen.TitleText, name="ALARM  :", value="", editable=False)
+        self.tmon = self.add(npyscreen.TitleText, name="T:", value="", editable=False, rely=11)
+        self.tev = self.add(npyscreen.TitleText, name="TEV:", value="", editable=False,)
+        self.compressorStatus = self.add(npyscreen.TitleText, name="COMPR:", value="", editable=False)
+        self.fanStatus = self.add(npyscreen.TitleText, name="FAN:", value="", editable=False)
+        self.defrostStatus = self.add(npyscreen.TitleText, name="DEFROST:", value="", editable=False)
+        self.alarm = self.add(npyscreen.TitleText, name="ALARM :", value="", editable=False)
         
         self.monitorSwitch = self.add(npyscreen.Checkbox, name = "Write to file", scroll_exit=True, rely=18)
         self.writeToFile = self.monitorSwitch.value
@@ -102,14 +103,17 @@ class ECP202ControlPanel(npyscreen.ActionForm):
     def while_waiting(self):
         if (self.counter%10==0):
             t=self.ecp202.getAmbientTemperature()
+            t_ev=self.ecp202.getEvaporatorTemperature()
             status=self.ecp202.getIOStatus()
             alarm=self.ecp202.getAlarmStatus()
             self.tmon.value="%3.1f C"%(t)
+            self.tev.value="%3.1f C"%(t_ev)
             self.compressorStatus.value="%d"%((status&self.ecp202.COMPR_STATUS_MASK)>0)
             self.fanStatus.value="%d"%((status&self.ecp202.FAN_STATUS_MASK)>0)
             self.defrostStatus.value="%d"%((status&self.ecp202.DEFROST_STATUS_MASK)>0)
             self.alarm.value="%d"%(alarm)
             self.tmon.display()
+            self.tev.display()
             self.compressorStatus.display()
             self.fanStatus.display()
             self.defrostStatus.display()
